1849032617f68459f9dbcb7d271c40e0ee6a7a18
zgabriellez <huhyana81@gmail.com>
2022-06-08 18:49:09 -0300

    corrected errors

--- complex-numbers/complex-numbers.jl
+++ complex-numbers/complex-numbers.jl
@@ -1,9 +1,9 @@
-import Base: +, -, *, /, ^, ==
+import Base: +, -, *, /, ^, ==, ≈, abs, conj, real, imag
 
-struct ComplexNumber
-    a
-    b
-    ComplexNumber(a,b) = new(a,b)
+struct ComplexNumber<:Number
+    a::Number
+    b::Number
+    ComplexNumber(a, b)= new(a,b)
 end
 
 function +(C::ComplexNumber, D::ComplexNumber)
@@ -18,13 +18,12 @@ end
 function abs(C::ComplexNumber)
     return (C.a^2 + C.b^2)^(1/2)
 end
-#(a + i * b) / (c + i * d) = (a * c + b * d)/(c^2 + d^2) + (b * c - a * d)/(c^2 + d^2) * i`
-function /(C::ComplexNumber, D::ComplexNumber)
-    return ComplexNumber((C.a * D.a + C.b * D.b)/(abs(D)^2), (C.b*D.a - C.a*D.b)/(abs(D)^2))
-end 
 function conj(C::ComplexNumber)
     return ComplexNumber(C.a, -C.b)
 end
+function /(C::ComplexNumber, D::ComplexNumber)
+    return ComplexNumber((C.a * D.a + C.b * D.b)/(abs(D)^2), (C.b*D.a - C.a*D.b)/(abs(D)^2))
+end 
 function real(C::ComplexNumber)
     return C.a
 end
@@ -34,3 +33,9 @@ end
 function exp(C::ComplexNumber)
     return ComplexNumber(Base.MathConstants.e^(C.a), 0)*ComplexNumber(cos(C.a), sin(C.b))
 end 
+function ==(C::ComplexNumber, D::ComplexNumber)
+    return isapprox(C.a, D.a) && isapprox(C.b, D.b)
+end
+function ≈(C::ComplexNumber, D::ComplexNumber)
+    return isapprox(C.a, D.a) && isapprox(C.b, D.b)
+end
